79c28e7fb0d5e264388bed4663f45ab0cecfc127
Picorovisck <daviavelino2007@gmail.com>
2024-05-21 06:44:02 -0300

    new teste

--- src/client-code.ts
+++ src/client-code.ts
@@ -6,6 +6,6 @@ const turma1 = turma.createStudents('Davi', 'avelino',17,3002)
 const turma2 = turma.createTeachers('Monica', 'ingles')
 
 console.log(turma1.infoStudent())
-console.log(`\n --------`);
+console.log(`\n -------`);
 console.log(turma2.infoTeacher());
 
