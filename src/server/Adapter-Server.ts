facebf14dde42505ea655bb4e244a539e2bce273
Picorovisck <daviavelino2007@gmail.com>
2024-05-26 07:31:50 -0300

    adapter incluse

--- src/server/Adapter-Server.ts
+++ src/server/Adapter-Server.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import { ServerProtocol } from "../interfaces/Adapter-Server-Protocol";
+import { route } from '../routes/routes';
 
 export class AdapterServer implements ServerProtocol {
  isRunning() {
         const app = express() 
         app.use(express.json())
-
+        app.use(route)
+        
          app.listen(3000, () => {
             console.log('server is running...');
         })
