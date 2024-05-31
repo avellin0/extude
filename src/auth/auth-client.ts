e9293263343814716fd31163b80c9695a6590f9d
Picorovisck <daviavelino2007@gmail.com>
2024-05-31 07:23:25 -0300

    middleware started

---
+++ src/auth/auth-client.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express'
+
+export {sign,verify} from 'jsonwebtoken'
+
+export const signIn = async (req: Request,res: Response) => {
+    try {
+        const {} = req.body
+
+        
+
+    } catch (error) {
+        
+    }
+}
