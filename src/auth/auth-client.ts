2520ffa71e2de030d86c8c27e7260463aee7965a
Picorovisck <daviavelino2007@gmail.com>
2024-06-04 08:10:14 -0300

    auth process

--- src/auth/auth-client.ts
+++ src/auth/auth-client.ts
@@ -3,19 +3,25 @@ import { Request, Response } from 'express'
 import { sign } from 'jsonwebtoken';
 
 export const signIn = async (req: Request,res: Response) => {
+        const {teacher_id} = req.body;
+
     try {
-        const {userid, name} = req.body;
+
+        const teachers_id = await fetchTeachers(teacher_id)
+        const teacher = await teachers_id.rows[0].teacher_id
+        const teacher_Access = await fetchTeachersAccess(teacher_id) 
+        const access = await teacher_Access.rows[0].role
 
         const MY_SECRET_KEY= process.env.MY_SECRET_KEY
-
+    
 
         if (!MY_SECRET_KEY) {
             throw new Error("Chave Secreta não fornecida!");
         }
 
         const token = sign({
-            userid: userid,
-            Access: name
+            userid: teacher,
+            Access: access
         },MY_SECRET_KEY,{
             algorithm: "HS256",
             expiresIn: "1h"
@@ -24,6 +30,23 @@ export const signIn = async (req: Request,res: Response) => {
         res.json(token)
 
     } catch (error) {
-        
+        console.error("Erro no servidor:", error);
+        res.status(500).json({ message: "Ocorreu um erro no servidor" });
+    }
+
+    async function fetchTeachers(id:any){
+        const result = await db.query('SELECT * FROM teachers WHERE teacher_id = $1', [id])
+        if(result.rows.length === 0){
+            throw new Error("Teacher's informations not found")
+        }
+        return result
+    }
+
+    async function fetchTeachersAccess(id:any){
+        const result = await db.query('SELECT role FROM teachers WHERE teacher_id = $1', [id])
+        if(result.rows.length === 0){
+            throw new Error("Teacher's informations not found")
+        }
+        return result
     }
 }
--- src/client-code.ts
+++ src/client-code.ts
@@ -11,15 +11,4 @@ function RunServerAdapter(server: ServerProtocol): void{
     }
 }
 
-
-const turma = new AbstractClassesFactory()
-
-const turma1 = turma.createStudents('Davi', 'avelino',17,3002)
-const turma2 = turma.createTeachers('Monica', 'ingles')
-
-console.log(turma1.infoStudent())
-console.log(`\n -------`);
-console.log(turma2.infoTeacher());
-
-
 RunServerAdapter(new AdapterServer())
