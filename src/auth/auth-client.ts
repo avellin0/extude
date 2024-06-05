e4d1906f7e6dd53b6d421cc9cadf8d2dfe3bc1cb
Picorovisck <daviavelino2007@gmail.com>
2024-06-05 07:36:17 -0300

    ainda não terminei essa merda

--- src/auth/auth-client.ts
+++ src/auth/auth-client.ts
@@ -1,18 +1,20 @@
+import { config } from "dotenv";
+config({path: "./Users/Plogl/Lessons/Projects/SchoolSchedule/src/.env"})
 import { db } from '../database/client-db';
 import { Request, Response } from 'express'
 import { sign } from 'jsonwebtoken';
 
 export const signIn = async (req: Request,res: Response) => {
-        const {teacher_id} = req.body;
 
     try {
 
+        const {teacher_id} = req.body;
+
         const teachers_id = await fetchTeachers(teacher_id)
         const teacher = await teachers_id.rows[0].teacher_id
-        const teacher_Access = await fetchTeachersAccess(teacher_id) 
-        const access = await teacher_Access.rows[0].role
+        const teacher_role = await fetchTeachersAccess(teacher_id)
 
-        const MY_SECRET_KEY= process.env.MY_SECRET_KEY
+        const MY_SECRET_KEY = process.env.MY_SECRET_KEY
     
 
         if (!MY_SECRET_KEY) {
@@ -20,21 +22,22 @@ export const signIn = async (req: Request,res: Response) => {
         }
 
         const token = sign({
-            userid: teacher,
-            Access: access
+            // userid: teacher,
+            // userRole: teacher_role.rows[0]
         },MY_SECRET_KEY,{
             algorithm: "HS256",
             expiresIn: "1h"
         })
 
-        res.json(token)
+        res.json({message: "hello"})
 
     } catch (error) {
         console.error("Erro no servidor:", error);
         res.status(500).json({ message: "Ocorreu um erro no servidor" });
     }
 
-    async function fetchTeachers(id:any){
+
+    async function fetchTeachers(id: number){
         const result = await db.query('SELECT * FROM teachers WHERE teacher_id = $1', [id])
         if(result.rows.length === 0){
             throw new Error("Teacher's informations not found")
@@ -42,7 +45,7 @@ export const signIn = async (req: Request,res: Response) => {
         return result
     }
 
-    async function fetchTeachersAccess(id:any){
+    async function fetchTeachersAccess(id: number){
         const result = await db.query('SELECT role FROM teachers WHERE teacher_id = $1', [id])
         if(result.rows.length === 0){
             throw new Error("Teacher's informations not found")
