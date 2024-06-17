7f87e192f79f8d74a134d1560d11a1d8427ddac8
Picorovisck <daviavelino2007@gmail.com>
2024-06-17 14:52:22 -0300

    CreateTeacher updated

--- src/controller/CreateTeachers.ts
+++ src/controller/CreateTeachers.ts
@@ -1,17 +1,21 @@
 import { Request, Response } from "express";
 import { Teacher } from "../CreateFactories/Teacher-factory";
-import { db } from "../database/client-db";
 
 export class CreateTeachers{
     async handle(req:Request,res:Response){
         const { userid, name, subject,access} = req.body;   
 
-        const new_teacher = new Teacher(name,subject,access,userid)
-        
-        const pushTeacher = await db.query('INSERT INTO teachers(teacher_id,name,subject,access) VALUES($1,$2,$3,$4);',[userid,name,subject,access])
+        const new_teacher = new Teacher()
+        new_teacher.createTeacher(userid,name,subject,access)
 
-        console.log(new_teacher);
-        return res.json(pushTeacher.rows)
+        const user = {
+            id: userid,
+            name: name,
+            tema: subject,
+            permissao: access
+        }
+
+        res.send(user)
     } 
 
  
