67d03a8d95f84ec8cfc1202a6bfccefa18cdc6ef
Picorovisck <daviavelino2007@gmail.com>
2024-06-19 18:46:51 -0300

    students done!

--- src/database/client-db.ts
+++ src/database/client-db.ts
@@ -1,10 +1,13 @@
+import { config } from "dotenv";
+config({path: "./src/.env"})
+
 import {Client} from 'pg'
 const db = new Client({
-    user: 'postgres',
-    host: 'localhost',
+    user: process.env.USER,
+    host: process.env.HOST,
     port: 5432,
-    database: 'school_schedule',
-    password: 'pico'
+    database: process.env.DATABASE,
+    password: process.env.PASS
 })
 
 db.connect()
