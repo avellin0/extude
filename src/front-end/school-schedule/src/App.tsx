e50212265dc2642538cc7c39c8afe66f141a5be7
Picorovisck <daviavelino2007@gmail.com>
2024-06-30 20:58:40 -0300

    updated with vite

---
+++ src/front-end/school-schedule/src/App.tsx
@@ -0,0 +1,51 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+    <header>
+      <h1>SmartSpace</h1>
+      <div className="cadastro">
+        <div className="btn_signIn">Sign In</div>
+        <div className="btn_signOut">Sign Out</div>
+      </div>
+    </header>
+    <main>
+      <div className="quadrado">
+        <div className="icone">👨🏼‍⚕️</div>
+        <div className="conteudo">
+          Salve vidas e faça a diferença. Torne-se um profissional de medicina e inspire o futuro!
+        </div>
+        <div className="footer">Mais informações</div>
+        <div className="overlay">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+      </div>
+      <div className="quadrado">
+        <div className="icone">👨🏼‍🎓</div>
+        <div className="conteudo">
+          Confira os outros cursos disponíveis e descubra novas oportunidades de aprendizado!
+        </div>
+        <div className="footer">Mais informações</div>
+        <div className="overlay">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+      </div>
+      <div className="quadrado">
+        <div className="icone">👨🏼‍💻</div>
+        <div className="conteudo">
+          Se torne um programador e transforme suas ideias em realidade. Comece sua jornada hoje!
+        </div>
+        <div className="footer">Mais informações</div>
+        <div className="overlay">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+      </div>
+      <div className="quadrado">
+        <div className="icone">👨🏼‍🔬</div>
+        <div className="conteudo">
+          Explore o mundo dos elementos e reações. Inicie sua carreira em Química e inove o amanhã!
+        </div>
+        <div className="footer">Mais informações</div>
+        <div className="overlay">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+      </div>
+    </main>
+  </div>
+  )
+}
+
+export default App
