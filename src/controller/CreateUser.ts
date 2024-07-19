c321ae04d7628d521330627286e7c1c4c48a7ec9
Picorovisck <daviavelino2007@gmail.com>
2024-07-19 20:40:37 -0300

    server is done

---
+++ src/controller/CreateUser.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import {createHash} from 'crypto'
+import {db} from '../database/client-db'
+export class CreateUser{
+    async handle(req:Request ,res:Response){
+            const {userId,name,email,password,permissions} = req.body
+
+            if(!userId || !name || !email || !password || !permissions){
+                return res.status(500).send('Todos os campos são obrigatorios')
+            }
+
+            const Hash = createHash('sha256').update(password).digest('hex')
+
+            try{
+
+            const create = await db.query('INSERT INTO Usuario(userId,name,email,password,permission) VALUES($1,$2,$3,$4,$5)', [userId,name,email,Hash,permissions])
+
+            res.send(create.rows)
+            
+            }catch(err){
+                console.log(err);
+                res.status(500).send('Erro interno')
+            }
+    }
+}
--- src/server/Adapter-Server.ts
+++ src/server/Adapter-Server.ts
@@ -8,8 +8,8 @@ export class AdapterServer implements ServerProtocol {
  isRunning() {
         const app = express()
 
-        app.use(cors)
-        app.use(bodyParser.urlencoded({extended: true}))
+        // app.use(cors)'
+        // app.use(bodyParser.urlencoded({extended: true}))
 
         app.use(express.json())
         app.use(route)
