258848cab15ec04e5bccd8631764515e139d6b23
Picorovisck <daviavelino2007@gmail.com>
2024-07-22 18:53:38 -0300

    cadastro feito

--- src/controller/CreateUser.ts
+++ src/controller/CreateUser.ts
@@ -3,19 +3,19 @@ import {createHash} from 'crypto'
 import {db} from '../database/client-db'
 export class CreateUser{
     async handle(req:Request ,res:Response){
-            const {userId,name,email,password,permissions} = req.body
+            const {name,email,password,permissions} = req.body
 
-            if(!userId || !name || !email || !password || !permissions){
-                return res.status(500).send('Todos os campos são obrigatorios')
+            if(!name || !email || !password || !permissions){
+                return res.status(400).send('Todos os campos são obrigatorios')
             }
 
             const Hash = createHash('sha256').update(password).digest('hex')
 
             try{
 
-            const create = await db.query('INSERT INTO Usuario(userId,name,email,password,permission) VALUES($1,$2,$3,$4,$5)', [userId,name,email,Hash,permissions])
+            const create = await db.query('INSERT INTO Usuario(name,email,password,permission) VALUES($1,$2,$3,$4)', [name,email,Hash,permissions])
             
-            res.status(201).json({ message: 'User created successfully', data: create.rows }).send(create.rows);
+            res.status(201).json({ message: 'User created successfully', data: create.rows })
 
             }catch(err){
                 console.log(err);
