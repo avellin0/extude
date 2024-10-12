8e984c0a04ddd1595ffd323791db760811644f91
Picorovisck <daviavelino2007@gmail.com>
2024-10-12 10:06:48 -0300

    agora ja pode buscar seus videos já baixados e ver

--- src/front-end/school-schedule/src/pages/AdmPage.tsx
+++ src/front-end/school-schedule/src/pages/AdmPage.tsx
@@ -1,25 +1,24 @@
-import video from '../videos/video_completo.mp4'
+// import video from '../videos/video_completo.mp4'
+
 import './Project.css'
-import {useState, useEffect} from 'react'
+import {useState} from 'react'
 
 export function AdmPage(){
 
-  const [text,setText] = useState("")
+    const [HaveVideo, setHaveVideo] = useState(true)
+    const [VideoUrl, setVideoUrl] = useState<string | null>(null)
+    const sendSubmit = () => {
+      const getFileNameId = document.getElementById('file_name') as HTMLInputElement
+     
+      if(!getFileNameId.files) return
 
-    useEffect(() => {
-      fetch('http://localhost:5000/text/economia.txt')
-      .then(response => {
-        if(response.ok){
-            return response.text()
-        }
+      const file = getFileNameId.files[0] 
 
-          throw('Dados não encontrado')
-      })
-      
-      .then(data => {
-        setText(data)
-      })
-    },[])
+      const videoUrl = URL.createObjectURL(file)
+
+      setVideoUrl(videoUrl)
+      setHaveVideo(false)
+    } 
 
     return (
         <>
@@ -27,19 +26,28 @@ export function AdmPage(){
 
           <div className="project-yt">
             
-            <div className="project-videos">
+                <div className="project-videos">
+                  { HaveVideo? (
+                    <>
+                      <h3><a href="">Baixe</a><br/></h3>
+                      <p>Ou</p>
+                      <input type="file" id={"file_name"} />
+                      <button onClick={() => sendSubmit()}>Enviar</button>
+                  </>
+                  )
+                  :
+                  (
+                  <>
+                  <video controls height="100%" id='project-video-style'>
+                  <source src={VideoUrl || ""} type="video/mp4" />
+                   Seu navegador não suporta o elemento de vídeo.
+                  </video>
+                  </>
+                  )  
+                }                    
             
-            <video controls height="100%">
-            <source src={video} type="video/mp4" />
-            Seu navegador não suporta o elemento de vídeo.
-            </video>
-            
-            </div>
-
-                <div className="project-transcricao">
-                    <textarea id="project-transcription-area" value={text} readOnly></textarea>
                 </div>
-            </div>
+          </div>
 
             <div className="project-notes-scope">
               <div className="project-notes">
