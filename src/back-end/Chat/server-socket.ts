565c44433f1e8391c24583f7c214ab11c0c1908c
Ðåvï åvêlïñð <daviavelino2007@gmail.com>
2025-05-30 16:21:45 -0300

    resolvendo problema com urls

--- src/back-end/Chat/server-socket.ts
+++ src/back-end/Chat/server-socket.ts
@@ -8,7 +8,7 @@ const app = express()
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://extude.vercel.app/",
         methods: ['GET','POST']
     }
 })
