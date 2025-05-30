2f6c6c5c33797bef82eb17183de5b68799a5262a
Ðåvï åvêlïñð <daviavelino2007@gmail.com>
2025-05-30 15:37:31 -0300

    include a db on render

--- src/back-end/database/client-db.ts
+++ src/back-end/database/client-db.ts
@@ -3,11 +3,7 @@ dotenv.config()
 
 import {Client} from 'pg'
 const db = new Client({
-    user: process.env.USER,
-    host: process.env.HOST,
-    port: Number(process.env.PORT),
-    database: process.env.DATABASE,
-    password: process.env.PASS,
+  connectionString: process.env.DATABASE_URL,
     ssl:{
         rejectUnauthorized: false
     }
